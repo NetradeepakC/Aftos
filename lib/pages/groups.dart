0e1fe68f19953ee37faa86fa2b1aac28d6d439e1
NetradeepakC <netradeepakc@gmail.com>
2022-06-04 13:05:18 +0530

    Added Group card

--- lib/pages/groups.dart
+++ lib/pages/groups.dart
@@ -1,6 +1,7 @@
 import "package:aftos/pages/standard_page.dart";
 import 'package:aftos/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:aftos/helpers.dart';
 
 class Groups extends StandardPage {
   Groups({
@@ -27,8 +28,15 @@ class GroupsState extends State<Groups> {
     return Column(
       children: [
         defaultAppBar(darkMode),
-        Center(
-          child: Text(widget.title),
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemBuilder: (BuildContext context, int index) {
+              return GroupCard(
+                url: Helpers.randomPictureUrl(),
+              );
+            },
+          ),
         ),
       ],
     );
--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -133,6 +133,92 @@ class ChatCard extends StatelessWidget {
   }
 }
 
+class GroupCard extends StatelessWidget {
+  const GroupCard({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  final String? url;
+
+  @override
+  Widget build(BuildContext context) {
+    bool darkMode = isDark(context);
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: defaultButtonStyle(darkMode),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text(
+                randomName(),
+                overflow: TextOverflow.fade,
+                style: const TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              vpad,
+              Divider(
+                thickness: 1,
+                endIndent: 0,
+                color: (darkMode) ? AppColors.textDark : AppColors.textLight,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: darkMode ? AppColors.textDark : AppColors.textLight,
+                  ),
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(18.0),
+                  child: Image(
+                    image: CachedNetworkImageProvider(url!),
+                  ),
+                ),
+              ),
+              vpad,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: defaultButtonStyle(
+                      darkMode,
+                      inverter: true,
+                    ),
+                    child: const Icon(Icons.thumb_up_alt_outlined),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: defaultButtonStyle(
+                      darkMode,
+                      inverter: true,
+                    ),
+                    child: const Icon(Icons.thumb_down_alt_outlined),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: defaultButtonStyle(
+                      darkMode,
+                      inverter: true,
+                    ),
+                    child: const Icon(Icons.mode_comment_outlined), //Icon
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class FriendCard extends StatelessWidget {
   const FriendCard({Key? key}) : super(key: key);
 
@@ -184,48 +270,6 @@ class FriendCard extends StatelessWidget {
                 ),
               ),
               hpad,
-              ElevatedButton(
-                onPressed: () {},
-                style: defaultButtonStyle(darkMode, inverter: true),
-                child: const Icon(Icons.favorite_border_outlined),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class GroupCard extends StatelessWidget {
-  const GroupCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    bool darkMode = isDark(context);
-    return Padding(
-      padding: const EdgeInsets.all(5),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: defaultButtonStyle(darkMode),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Avatar.large(url: Helpers.randomPictureUrl()),
-              hpad,
-              SizedBox(
-                width: 150,
-                child: Text(
-                  randomName(),
-                  style: const TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  overflow: TextOverflow.fade,
-                ),
-              ),
-              const Spacer(),
               ElevatedButton(
                 onPressed: () {},
                 style: defaultButtonStyle(darkMode, inverter: true),
